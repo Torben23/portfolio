f7c5cb8341c5edf06e7dac14a0a7bd6e0b585f36
Torben <105077862+Torben23@users.noreply.github.com>
2023-04-21 03:14:43 -0300

    Add files via upload

---
+++ src/scenes/MySkills.jsx
@@ -0,0 +1,157 @@
+import LineGradient from "../components/LineGradient";
+import useMediaQuery from "../hooks/useMediaQuery";
+import { motion } from "framer-motion";
+
+const MySkills = () => {
+  const isAboveLarge = useMediaQuery("(min-width: 1060px)");
+  return (
+    <section id="skills" className="pt-10 pb-24">
+      {/* HEADER AND IMAGE SECTION */}
+      <div className="md:flex md:justify-between md:gap-16 mt-32">
+        <motion.div
+          className="md:w-1/3"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className="font-playfair font-semibold text-4xl mb-5">
+            MY <span className="text-red">SKILLS</span>
+          </p>
+          <LineGradient width="w-1/3" />
+          <p className="mt-10 mb-7">
+            I began studying Systems Engineering while working local jobs in technical informatic. Now, after taking four exceptional courses of over 1200 hours, i gained the experience in working in the following technologies:
+          </p>
+        </motion.div>
+
+        <div className="mt-16 md:mt-0">
+          {isAboveLarge ? (
+            <div
+              className="relative z-0 ml-20 before:absolute before:-top-10 before:-left-10
+              before:w-full before:h-full before:border-2 before:border-blue before:z-[-1]"
+            >
+              <img
+                alt="skills"
+                className="z-10"
+                src="assets/skills-image.png"
+              />
+            </div>
+          ) : (
+            <img alt="skills" className="z-10" src="assets/skills-image.png" />
+          )}
+        </div>
+      </div>
+
+      {/* SKILLS */}
+      <div className="md:flex md:justify-between mt-16 gap-32">
+        {/* Responsive Web Design */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">Responsive</p>
+              <p className="font-playfair font-semibold text-3xl mt-3">
+              Web Design
+              </p>
+            </div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]" />
+          </div>
+          <p className="mt-5">
+            HTML for content, and CSS for design.
+          </p>
+        </motion.div>
+
+        {/* JS Algorithms & Data Structures */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">Javascript</p>
+              <p className="font-playfair font-semibold text-3xl mt-3">
+                 & Data Structures
+              </p>
+            </div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-red absolute right-0 top-0 z-[-1]" />
+          </div>
+          <p className="mt-5">
+            Fundamentals of JavaScript including variables, arrays, objects, loops, and functions.
+          </p>
+        </motion.div>
+        {/* Front End Development Libraries */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">Front End</p>
+              <p className="font-playfair font-semibold text-3xl mt-3">
+                Libraries
+              </p>
+            </div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-yellow absolute right-0 top-0 z-[-1]" />
+          </div>
+          <p className="mt-5">
+             Bootstrap, jQuery, Sass, React & Redux.
+          </p>
+        </motion.div>
+        {/* Back End Development and APIs */}
+        <motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="relative h-32">
+            <div className="z-10">
+              <p className="font-playfair font-semibold text-5xl">Back End</p>
+              <p className="font-playfair font-semibold text-3xl mt-3">
+                & APIs
+              </p>
+            </div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-dark-grey absolute right-0 top-0 z-[-1]" />
+          </div>
+          <p className="mt-5">
+            Node.js, Express framework, MongoDB and the Mongoose library.
+          </p>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default MySkills;
---
+++ src/scenes/Projects.jsx
@@ -0,0 +1,147 @@
+import LineGradient from "../components/LineGradient";
+import { motion } from "framer-motion";
+
+const container = {
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const projectVariant = {
+  hidden: { opacity: 0, scale: 0.8 },
+  visible: { opacity: 1, scale: 1 },
+};
+
+const Project = ({ title, resume }) => {
+  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
+    bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
+  const projectTitle = title.split(" ").join("-").toLowerCase();
+
+  return (
+    <motion.div variants={projectVariant} className="relative">
+      <div className={overlayStyles}>
+        <p className="text-2xl font-playfair">{title}</p>
+        <p className="mt-7">
+          {resume}
+        </p>
+      </div>
+      <img src={`../assets/${projectTitle}.jpeg`} alt={projectTitle} />
+    </motion.div>
+  );
+};
+
+const Projects = () => {
+  return (
+    <section id="projects" className="pt-48 pb-48">
+      {/* HEADINGS */}
+      <motion.div
+        className="md:w-2/5 mx-auto text-center"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, y: -50 },
+          visible: { opacity: 1, y: 0 },
+        }}
+      >
+        <div>
+          <p className="font-playfair font-semibold text-4xl">
+            <span className="text-red">PRO</span>JECTS
+          </p>
+          <div className="flex justify-center mt-5">
+            <LineGradient width="w-2/3" />
+          </div>
+        </div>
+        <p className="mt-10 mb-10">
+          Those proyects has enlightened me, and i look forward in expanding my knowledge by accepting new challenges thay would help me grow in my career. Eventually, i see myself in leader role were i can use my experience and skills to help others and achieve new goals.
+        </p>
+      </motion.div>
+
+      {/* PROJECTS */}
+      <div className="flex justify-center">
+        <motion.div
+          className="sm:grid sm:grid-cols-3"
+          variants={container}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.2 }}
+        >
+          {/* ROW 1 */}
+          
+            <a href={`https://mobile-app.marcosberton.repl.co`} target="_blank" rel="noopener noreferrer">
+           <Project title="Project 1" resume="Mobile App Navigation"  />
+            </a>
+                      
+            <a href={`https://Menu.marcosberton.repl.co`} target="_blank" rel="noopener noreferrer">
+          <Project title="Project 2" resume="Restaurant menu" />
+            </a>
+              
+           <a href={`https://ia-images.marcosberton.repl.co`} target="_blank" rel="noopener noreferrer">
+          <Project title="Project 3" resume="IA images with they respective prompt" />
+            </a>
+             
+          {/* ROW 2 */}
+          
+            <a href={`https://Form-Imput.marcosberton.repl.co`} target="_blank" rel="noopener noreferrer">  
+          <Project title="Project 4" resume="Login and registration form in HTML, CSS & JavaScript" />
+            </a>
+          
+             <a href={`https://expanding-img.marcosberton.repl.co/`} target="_blank" rel="noopener noreferrer">
+           <Project title="Project 5" resume="Expanding Cards with HTML, CSS & JS"  />
+            </a> 
+                        
+              
+          <a href={`https://back-images.marcosberton.repl.co/`} target="_blank" rel="noopener noreferrer">
+          <Project title="Project 6" resume="React JS Background slider" />
+            </a>
+
+            {/* ROW 3 */}
+          
+          <a href={`https://clock.marcosberton.repl.co`} target="_blank" rel="noopener noreferrer">
+          <Project title="Project 7" resume="React Js Analog Clock" />
+            </a>
+
+          <a href={`https://todolist.marcosberton.repl.co`} target="_blank" rel="noopener noreferrer">
+           <Project title="Project 8" resume="'To do list' to organize your work & life"  />
+            </a>
+
+          <a href={`https://Draw.marcosberton.repl.co`} target="_blank" rel="noopener noreferrer">
+           <Project title="Project 9" resume="Simple drawing app"  />
+            </a>
+
+          {/* ROW 4 */}
+
+          <a href={`https://LinkBand.marcosberton.repl.co`} target="_blank" rel="noopener noreferrer">
+          <Project title="Project 10" resume="Social links for a band" />
+          </a>
+            
+           <a href={`https://Quiz-App.marcosberton.repl.co`} target="_blank" rel="noopener noreferrer">
+           <Project title="Project 11" resume="Quiz app" />
+            </a>
+          
+          <a href={`https://Live-User-Filter.marcosberton.repl.co`} target="_blank" rel="noopener noreferrer">
+           <Project title="Project 12" resume="Live User Filter"  />
+            </a>
+           {/* ROW 5 */}
+          <a href={`https://Calculator.marcosberton.repl.co`} target="_blank" rel="noopener noreferrer">
+           <Project title="Project 13" resume="React Js calculator with MathJs library"  />
+            </a>
+          <a href={`https://Bateria.marcosberton.repl.co`} target="_blank" rel="noopener noreferrer">
+          <Project title="Project 14" resume="Electronic drum. You can play it with mouse or keyboard." />
+            </a>
+
+          <a href={`https://Frases.marcosberton.repl.co`} target="_blank" rel="noopener noreferrer">
+          <Project title="Project 15" resume="Random quote machine" />
+            </a>
+
+          </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
